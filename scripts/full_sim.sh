ae11de31696d880c3d75b01a03a40dea5fe75293
Michael Beavitt <mbeavitt@gmail.com>
2025-09-24 17:18:33 +0100

    fixed typo

--- scripts/full_sim.sh
+++ scripts/full_sim.sh
@@ -7,7 +7,7 @@ python -m censim.simulation \
     ./data/15000copy_cen178.seq 1000 \
     ./data/15000copy_cen178.178bp.bed.pos \
     ./output/fasta/1000generation.out.fa \
-    ./ouptut/records/1000generation.record.txt \
+    ./output/records/1000generation.record.txt \
     ./output/generation_unit_pos/1000generation.unit.pos
 
 ## Step 2: Continue Simulation (to 6 million generations)
